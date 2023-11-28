05f19789c538fdff4b3752dc0d4ca23fd09e8d0e
Marcin Bujak <marcin.bujak@polskaligaesportowa.pl>
2023-11-28 18:39:28 +0100

    Dodanie pytan

--- src/data/test-data.ts
+++ src/data/test-data.ts
@@ -4,43 +4,127 @@ export default {
     title: "Test wiedzy ogólnej",
     questions: [{
         timeSpent: 0,
-        question: "Jakie miasto jest stolicą Polski?",
-        correctAnswer: "Warszawa",
+        question: "Który pierwiastek chemiczny jest oznaczany symbolem 'O'?",
+        correctAnswer: "Tlen",
         answers: [{
-            content: "Warszawa",
+            content: "Węgiel",
             id: "first"
         },
         {
-            content: "Kraków",
+            content: "Tlen",
             id: "second"
         },
         {
-            content: "Gniezno",
+            content: "Wodór",
             id: "third"
         },
         {
-            content: "Lubin",
+            content: "Azot",
             id: "fourth"
         }]
     },
     {
         timeSpent: 0,
-        question: "Co nie jest dzielnicą Lublina?",
-        correctAnswer: "Pruszków",
+        question: "Które państwo jest największe pod względem powierzchni na świecie?",
+        correctAnswer: "Rosja",
         answers: [{
-            content: "Pruszków",
+            content: "Chiny",
             id: "first"
         },
         {
-            content: "Wrotków",
+            content: "Stany Zjednoczone",
             id: "second"
         },
         {
-            content: "Śródmieście",
+            content: "Rosja",
             id: "third"
         },
         {
-            content: "Tatary",
+            content: "Polska",
+            id: "fourth"
+        }]
+    }
+    ,{
+        timeSpent: 0,
+        question: "Kto wygrał worldsy 2023 w LoL?",
+        correctAnswer: "SKT T1",
+        answers: [{
+            content: "SKT T1",
+            id: "first"
+        },
+        {
+            content: "GenG",
+            id: "second"
+        },
+        {
+            content: "JDG",
+            id: "third"
+        },
+        {
+            content: "Weibo",
+            id: "fourth"
+        }]
+    }
+    ,{
+        timeSpent: 0,
+        question: "Ile planet krąży wokół Słońca w naszym Układzie Słonecznym?",
+        correctAnswer: "8",
+        answers: [{
+            content: "8",
+            id: "first"
+        },
+        {
+            content: "9",
+            id: "second"
+        },
+        {
+            content: "10",
+            id: "third"
+        },
+        {
+            content: "7",
+            id: "fourth"
+        }]
+    }
+    ,{
+        timeSpent: 0,
+        question: "Jaki jest najwyższy szczyt na świecie?",
+        correctAnswer: "Mount Everest",
+        answers: [{
+            content: "Mont Blanc",
+            id: "first"
+        },
+        {
+            content: "K2",
+            id: "second"
+        },
+        {
+            content: "Mount Everest",
+            id: "third"
+        },
+        {
+            content: "Śnieżka",
+            id: "fourth"
+        }]
+    }
+    ,{
+        timeSpent: 0,
+        question: "W którym roku odbyła się pierwsza podróż człowieka w kosmos?",
+        correctAnswer: "1961",
+        answers: [{
+            content: "1957",
+            id: "first"
+        },
+        {
+            content: "1958",
+            id: "second"
+        },
+        {
+            content: "1960",
+            id: "third"
+        },
+        {
+            content: "1961",
             id: "fourth"
         }]
     }]
